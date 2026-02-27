043975319bdb0fe51f9d82bfd039beb327066562
hannahaw <142659698+hannahaw@users.noreply.github.com>
2026-02-27 12:27:43 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import codecs
+
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+    with codecs.open(html_file, 'r', 'utf-8') as file:
+        html = file.read()
+
+    clean_text = ""
+    inside_tag = False
+
+    for char in html:
+        if char == '<':
+            inside_tag = True
+        elif char == '>':
+            inside_tag = False
+        elif not inside_tag:
+            clean_text += char
+
+    final_lines = []
+    for line in clean_text.split('\n'):
+        stripped_line = line.strip()
+        if stripped_line:
+            final_lines.append(stripped_line)
+
+    with codecs.open(result_file, 'w', 'utf-8') as file:
+        file.write('\n'.join(final_lines))
